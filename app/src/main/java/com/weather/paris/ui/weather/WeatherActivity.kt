28ccc00c13bdd5a734f2f79a952baf488b899b35
sallukp <salmon1989@gmail.com>
2020-08-02 18:40:57 +0200

    [DI][HILT] DatabaseModule and NetworkModule are added for dependency injection using dagger hilt library

--- app/src/main/java/com/weather/paris/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/weather/paris/ui/weather/WeatherActivity.kt
@@ -3,7 +3,9 @@ package com.weather.paris.ui.weather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.weather.paris.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
